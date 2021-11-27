60164469a24ef1cb8d06ebb7f68ebc8ceadefdbd
hadekproject <ing.harolddiazc@gmail.com>
2021-11-26 20:29:39 -0500

    primer commit

---
+++ casafirme/script.js
@@ -0,0 +1,14 @@
+/*const menuToggle = document.querySelector('.toggle');
+const menuNav = document.querySelector('.menunav');
+
+function toggleMenu(){
+
+    menuToggle.classList.toggle('active');
+    menuNav.classList.toggle('active');
+    menuNav.style.opacity = '1';
+}
+
+function hiddeMenu(){
+    menuNav.style.opacity = '0';
+}
+*/
